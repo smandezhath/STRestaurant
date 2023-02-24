452a0caa066d5d16747aeb21adfd933379b891bb
hitha-nair <hithanair1@gmail.com>
2023-02-24 11:12:48 +0530

    done

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { Container } from '@mui/system';
 import { TextField, Button } from '@mui/material';
 import displaypage from "./components/displaypage";
 import Login from './components/login';
+import { BrowserRouter as Router, Switch,Route, Routes, Navigate, Redirect, } from "react-router-dom";
 function App() {
   return (
     <Routes>
 			{user && <Route path="/" exact element={<Login />} />}
 			<Route path="/signup" exact element={<Signup />} />
-			<Route path="/login" exact element={<Login />} />
+			<Route path="/displaypage" exact element={<displaypage />} />
 		  <Route path="/" element={<Navigate replace to="/login" />} />
 		</Routes>
 
